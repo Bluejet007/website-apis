8f917d8d7eafd6ec016015584076cc9eddbb8d7c
Bluejet007 <rachit.takate@gmail.com>
2025-07-07 10:26:29 +0530

    Added input blob cleanup

--- ImageFilters/JobProcessor.cs
+++ ImageFilters/JobProcessor.cs
@@ -1,3 +1,4 @@
+using Azure.Storage.Blobs;
 using ImageFilters.Common;
 using ImageFilters.Kuwahara;
 using Microsoft.Azure.Functions.Worker;
@@ -16,30 +17,35 @@ public class JobProcessor
 
     [Function(nameof(JobProcessor))]
     [BlobOutput("outputs/{fileName}")]
-    public byte[] Run([QueueTrigger("image-jobs", Connection = "AzureWebJobsStorage")] QueuedJob job, [BlobInput("inputs/{fileName}")] Stream inputStream)
+    public byte[] Run([QueueTrigger("image-jobs", Connection = "AzureWebJobsStorage")] QueuedJob job, [BlobInput("inputs/{fileName}")] BlobClient inputBlob)
     {
         _logger.LogInformation($"C# Queue trigger function processed: {job.FileName}, {job.JobType}, {job.Parameters}");
+        MemoryStream outputStream;
 
-        _logger.LogInformation("Converting to bytes");
-        ByteImage byteImage = new ByteImage(inputStream);
-        _logger.LogInformation("Conversion completed");
-
-        _logger.LogInformation("Processing image");
-        byteImage = ColourSpace.SrgbToLinear(byteImage);
-        ByteImage outputImage = job.JobType switch
+        using (Stream inputStream = inputBlob.OpenRead())
         {
-            JobType.greyscale => CommonFilters.GreyScale(byteImage),
-            JobType.baseKuwa => KuwaharaFilters.BaseKuwahara(byteImage, (byte)job.Parameters![0], _logger),
-            _ => byteImage
-        };
-        outputImage = ColourSpace.LinearToSrgb(outputImage);
-        _logger.LogInformation("Processing completed");
+            _logger.LogInformation("Converting to bytes");
+            ByteImage byteImage = new(inputStream);
+            _logger.LogInformation("Conversion completed");
 
-        _logger.LogInformation("Converting to stream");
-        MemoryStream outputStream = outputImage.ToMemoryStream();
-        _logger.LogInformation("Conversion completed");
+            _logger.LogInformation("Processing image");
+            byteImage = ColourSpace.SrgbToLinear(byteImage);
+            ByteImage outputImage = job.JobType switch
+            {
+                JobType.greyscale => CommonFilters.GreyScale(byteImage),
+                JobType.baseKuwa => KuwaharaFilters.BaseKuwahara(byteImage, (byte)job.Parameters[0]),
+                _ => byteImage
+            };
+            outputImage = ColourSpace.LinearToSrgb(outputImage);
+            _logger.LogInformation("Processing completed");
 
-        GC.Collect(GC.MaxGeneration, GCCollectionMode.Optimized, false, true);
+            _logger.LogInformation("Converting to stream");
+            outputStream = outputImage.ToMemoryStream();
+            _logger.LogInformation("Conversion completed");
+        }
+
+        inputBlob.DeleteIfExists();
+
         return outputStream.ToArray();
     }
 }
